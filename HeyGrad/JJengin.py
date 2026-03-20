c71725dfe110365f7cc457538ddc3f2b87e305b2
jq <jq@gmail.com>
2026-03-21 00:27:42 +0800

    add my grad

---
+++ HeyGrad/JJengin.py
@@ -0,0 +1,52 @@
+class Value:
+    def __init__(self, value, children=(), op=""):
+        self.data = value
+        self.prev = children
+        self._op = op
+        self.grad = 0
+        self._backward = lambda: None
+
+    def __add__(self, other):
+        other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other, children=(self, other), op="+")
+
+        def backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        self._backward = backward
+
+        return out
+
+    def __mul__(self, other):
+        other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other, children=(self, other), op="*")
+
+        def backward():
+            self.grad = other.data * out.grad
+            other.grad = self.data * out.grad
+
+        self._backward = backward
+
+        return out
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __repr__(self):
+        return f"Value({self.data})"
+
+    def backward(self):
+
+        topo = []
+        visited = set()
+
+        def buildTopo(v):
+            if v not in visited:
+                visited.add(v)
+                for c in v.prev:
+                    buildTopo(c)
+                topo.append(v)
